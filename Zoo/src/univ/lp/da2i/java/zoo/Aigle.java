57e6b6943a77d2da359beaae02a6b50ec453ad21
clariond <clement.clariond@hotmail.com>
2016-11-05 18:43:14 +0100

    javadoc aigle

--- Zoo/src/univ/lp/da2i/java/zoo/Aigle.java
+++ Zoo/src/univ/lp/da2i/java/zoo/Aigle.java
@@ -1,36 +1,80 @@
 package univ.lp.da2i.java.zoo;
 
-public abstract class Aigle extends Oiseau {
+/**
+ * La classe aigle représente
+ * 
+ * @author Administrateur
+ * 
+ * @see Oiseau
+ *
+ */
 
+public abstract class Aigle extends Oiseau {
+	/**
+	 * Constructeur de Aigle
+	 * 
+	 * On utilise le constructeur de la classe mére oiseau
+	 * 
+	 * @param name	
+	 * 		Le nom de l'aigle
+	 * @param poids
+	 * 		son poids
+	 * @param taille
+	 * 		sa taille
+	 * @param age
+	 * 		son l'age	
+	 * @param isHungry
+	 * 		son indicateur de faim	
+	 * @param isSleeping
+	 * 		son indicateur de sommeil (si il dort)
+	 * @param isHealty
+	 * 		son idicateur de soin (si il est en train d'etre soigné ou non)
+	 * 
+	 */
 	public Aigle(String name, double poids, double taille, int age, boolean isHungry, boolean isSleeping,
 			boolean isHealthy) {
 		super(name, poids, taille, age, isHungry, isSleeping, isHealthy);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	/**
+	 * fait voler l'aigle
+	 */
 	@Override
 	public void voler() {
 		System.out.println("Je vole comme un aigle !");
 	}
-
+	
+	/**
+	 * fait manger l'aigle
+	 */
 	@Override
 	public void manger() {
 		System.out.println("Je mange comme un aigle");
 
 	}
-
+	/**
+	 * fait emettre un son a l'aigle
+	 */
 	@Override
 	public void emettreUnSon() {
 		System.out.println("J'Ã©met un son comme un aigle");
 
 	}
-
+	/**
+	 * soigne l'aigle
+	 */
 	@Override
 	public void etreSoigne() {
 		System.out.println("Je me soigne");
 		this.setHealthy(true);
 	}
-
+	/**
+	 * reveille ou endort l'aigle
+	 * @param	b
+	 * 		endors l'aigle si vaut "true"
+	 * 		reveille l'aigle si vaut "false"
+	 */
 	@Override
 	public void changeEtat(boolean b) {
 		System.out.println("Je change d'Ã©tat");
